4e2ebcc094bc6b21dcebf3e35b0e1f9a6040c1d3
Julia <j.jakubowska06@gmail.com>
2021-08-31 18:23:27 +0200

    update on travel charts

--- src/charts/travel_data_final.py
+++ src/charts/travel_data_final.py
@@ -13,18 +13,18 @@ from travel_dataframe import travel_dataframe_generator
 
 def main(output: typing.Optional[str], prefix: str, table: bool): #, file):
         dispatch_rates = ['2_dispatch','4_dispatch','6_dispatch']
-        emergency_dispatch_rates = ['150_emergency', '300_emergency', '600_emergency']
+        emergency_dispatch_rates = ['300_emergency']
         
         for emergency_dispatch in emergency_dispatch_rates:
             for dispatch_rate in dispatch_rates:
-                tmp = str(output)+'/'+emergency_dispatch+'/'+dispatch_rate
+                tmp = str(output)
                 travel_dataframe_generator(tmp)
                 df = travel_dataframe_generator(tmp)
         
                 key = 'travel'
                 ylabel = 'Travel time (steps)'  # noqa: W605
                 multiplier = 1
-                ylim = 400
+                ylim = 750
                 
                 make_plot(df, key, ylabel, multiplier, ylim, table, tmp, prefix, emergency=False)
                 make_plot(df, key, ylabel, multiplier, ylim, table, tmp, prefix, emergency=True)
